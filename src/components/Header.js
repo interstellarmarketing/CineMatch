93c59ecd5ae89ac2b982f6ff56f417abb42c82e7
sankaraxi <sankarfrompalani@gmail.com>
2024-10-03 21:40:07 +0530

    initialized project files

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <div>
+        <div>
+            <img 
+                src="https://i.ibb.co/xgWPhvW/flimnest-logo.png"
+                alt="flimnest-logo" 
+            />
+        </div>
+    </div>
+  )
+}
+
+export default Header
