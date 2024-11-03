45bf87f3007d05b85c610870077105679dc6d64c
sankaraxi <sankarfrompalani@gmail.com>
2024-11-03 15:45:04 +0530

    enhance MovieDetails component with responsive design improvements and additional action buttons

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -143,37 +143,51 @@ const MovieDetails = () => {
             </div>
 
 
-            <div className="border-t-[1px] border-b-[1px] my-4 border-white">
-              <h1 className="text-2xl pt-2 font-semibold">Intrigued by <span className="font-bold">{movieDetails.title}</span>? Let’s dive into its world!</h1>
-              <div className="flex items-center justify-between">
-                <div className="">
-                  <Link to={movieDetails.homepage} target="_blank">
-                    <button className="flex gap-1 items-center justify-center text-2xl text-white font-bold p-2 mt-2 ">Website<span><FaGlobe/></span></button>
-                  </Link>
+            <div className="border-t-[1px] md:border-b-[1px] my-4 border-white max-md:mx-3">
+              <h1 className="text-lg md:text-2xlpt-2 font-semibold">Intrigued by <span className="font-bold">{movieDetails.title}</span>? Let’s dive into its world!</h1>
+              <div className="">
+                <div className="hidden md:flex items-center md:justify-between">
+                  <div className="">
+                    <Link to={movieDetails.homepage} target="_blank">
+                      <button className="flex gap-1 items-center justify-center text-2xl max-md:text-lg text-white font-bold p-2 mt-2 ">Website<span><FaGlobe/></span></button>
+                    </Link>
+                  </div>
+
+                  <div className="">
+                    <Link to={GOOGLE_URL + encodeURIComponent(movieDetails.title)} target="_blank">
+                      <button className="text-white font-bold mt-2 text-4xl max-md:text-xl md:pl-9 "><FcGoogle/></button>
+                    </Link>
+                  </div>
+
+                  <div className="">
+                    <Link to={IMDB_URL +movieDetails.imdb_id} target="_blank">
+                      <button className="text-white font-bold mt-2 text-6xl max-md:text-3xl "><LiaImdb /></button>
+                    </Link>
+                  </div>
+
+                  <div className="">
+                    <button onClick={scrollToTrailer} className="flex gap-1 items-center justify-center text-2xl text-white font-bold p-2 mt-2 ">Trailer<span className="text-3xl"><IoPlayOutline /></span></button>
+                  </div>
+
+                  <div className="">
+                    <button className="flex gap-1 items-center justify-center text-2xl text-white font-bold p-2 mt-2 ">Netflix</button>
+                  </div>
+
                 </div>
 
-                <div className="">
-                  <Link to={GOOGLE_URL + encodeURIComponent(movieDetails.title)} target="_blank">
-                    <button className="text-white font-bold mt-2 text-4xl pl-9 "><FcGoogle/></button>
-                  </Link>
-                </div>
-
-                <div className="">
-                  <Link to={IMDB_URL +movieDetails.imdb_id} target="_blank">
-                    <button className="text-white font-bold mt-2 text-6xl "><LiaImdb /></button>
-                  </Link>
-                </div>
-
-                <div className="">
+                <div className="flex md:hidden">  
+                  <div className="">
+                    
+                    <button onClick={scrollToTrailer} className="flex gap-1 items-center justify-center text-2xl text-white font-bold p-2 mt-2 ">Trailer<span className="text-3xl"><IoPlayOutline /></span></button>
                   
-                  <button onClick={scrollToTrailer} className="flex gap-1 items-center justify-center text-2xl text-white font-bold p-2 mt-2 ">Trailer<span className="text-3xl"><IoPlayOutline /></span></button>
+                  </div>
+
+                  <div className="">
+                    <button className="flex gap-1 items-center justify-center text-2xl text-white font-bold p-2 mt-2 ">Netflix</button>
+                  </div>
+
+                </div>
                 
-                </div>
-
-                <div className="">
-                  <button className="flex gap-1 items-center justify-center text-2xl text-white font-bold p-2 mt-2 ">Netflix</button>
-                </div>
-
               </div>
               
             </div>
