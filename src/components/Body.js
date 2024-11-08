aaa0732cb7e50f6438496806d38d53515d551a53
sankaraxi <sankarfrompalani@gmail.com>
2024-11-08 12:01:15 +0530

    enhance Body component with user-based navigation links and improved layout; add background image and responsive text elements

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,30 @@
 import { Link } from "react-router-dom";
 import Header from "./Header";
+import { MdKeyboardDoubleArrowRight } from "react-icons/md";
+import { SiGooglegemini } from "react-icons/si";
+import { useSelector } from "react-redux";
+
 
 const Body = () => {
+
+  const user = useSelector(store => store.user);
+  
+
   return (
-    <div className="pt-96 bg-gray-800" >
-        <h1 className="text-6xl">Sankar</h1>
-        <Link to="/browse" className="text-2xl">Browse</Link>
-        <Link to="/login" className="text-2xl">Login</Link>
+    <div className="bg-gray-800" >
+        <div className="relative flex ">
+          <img src="https://utfs.io/f/0Gl64F1LqW8ASxldPqUfmCFVWPrR6B3Dn9yoKa2jLgY0Sexd  " className="w-full h-screen object-cover" alt="Home Page" />
+        </div>
+
+        <div className="absolute inset-0 -top-24 flex flex-col items-center justify-center text-white">
+          <h1 className="text-xl  md:text-4xl text-center font-bold">Discover Hidden Gems and Blockbusters Made for You!</h1>
+          <div className="flex gap-3 items-center justify-center pt-3 text-black font-semibold">
+            <Link to={ user ? "/gptsearch" : "/login"} className="flex justify-center items-center gap-1 text-xl bg-white py-2 px-4 hover:bg-sky-400  hover:text-black rounded-sm"><span className='text-xl'><SiGooglegemini /></span><span>Gemini Search</span></Link>  
+            <Link to="/browse" className=" flex items-centert text-xl text-white py-2 px-4 hover:bg-white hover:text-sky-400 rounded-sm">Browse<span className="text-3xl"><MdKeyboardDoubleArrowRight /></span></Link>  
+          </div>
+
+        </div>
+        
     </div>
   )
 }
