87f56438af8c1af3cf9b64234bd3419772aa97a8
sankaraxi <sankarfrompalani@gmail.com>
2024-11-23 22:16:49 +0530

    refactor CategoryShimmer component; adjust width for improved layout

--- src/components/Shimmer/CategoryShimmer.js
+++ src/components/Shimmer/CategoryShimmer.js
@@ -4,7 +4,7 @@ const CategoryShimmer = () => {
         {Array.from({ length: 10 }).map((_, index) => (
           <div
             key={index}
-            className="w-[140px] md:w-56 p-1 md:h-[280px] md:p-2 bg-gray-700 rounded-md animate-pulse"
+            className="w-[100px] h-[280px] md:w-56 p-1 md:h-[280px] md:p-2 bg-gray-700 rounded-md animate-pulse"
           >
             <div className="w-full h-[220px] bg-gray-600 rounded-md"></div>
             <div className="mt-2 h-4 bg-gray-600 rounded"></div>
