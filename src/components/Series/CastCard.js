b61f9fa6757b6ddface410b6e7569e647059f7e8
sankaraxi <sankarfrompalani@gmail.com>
2024-12-01 16:21:43 +0530

    update CastCard component for centered text alignment, adjust VideoTitle positioning, and modify trending movies API fetch to include pagination

--- src/components/Series/CastCard.js
+++ src/components/Series/CastCard.js
@@ -8,8 +8,8 @@ const CastCard = ({actorName, actorCharacter, actorProfilePath}) => {
             src = {actorProfilePath ? `https://image.tmdb.org/t/p/w500${actorProfilePath}` : ACTOR_BANNER} 
             className="w-28px h-[168px] rounded-full object-contain"
         />
-        <h1 className="text-lg font-semibold">{actorName}</h1>
-        <h2 className="text-sm text-gray-400">{actorCharacter}</h2>
+        <h1 className="text-lg text-center font-semibold">{actorName}</h1>
+        <h2 className="text-sm text-center text-gray-400">{actorCharacter}</h2>
         
     </div>
   )
